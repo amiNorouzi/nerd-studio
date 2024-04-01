5f7fd06c6af82b9ad917ce457396c13ef3f63bb1
Amin <Amin@nematifamilyfundation.onmicrosoft.com>
2024-04-01 14:36:59 +0330

    CREATE: event chanel service

---
+++ src/services/events-chanel/index.ts
@@ -0,0 +1,34 @@
+import {useUserProfile} from '@/services/users';
+import {useEffect, useRef, useState} from 'react';
+
+type EventChanelParams = {
+    eventName: string
+    onMessage?: (message: string) => void
+}
+
+export function useEventChanel({onMessage, eventName}: EventChanelParams) {
+    const [message, setMessage] = useState('');
+    const eventSource = useRef<EventSource>();
+
+    useEffect(() => {
+        if (!eventSource.current) {
+            eventSource.current = new EventSource('http://5.78.55.161:8000/events/');
+            eventSource.current.addEventListener(eventName, (event) => {
+                if (event.data) {
+                    const data = JSON.parse(JSON.parse(event.data));
+                    onMessage?.(data.content);
+                    if (data.content)
+                        setMessage((prev) => prev + data.content);
+                }
+            });
+        }
+    }, [eventName, onMessage]);
+
+    return message;
+}
+
+const eventChanelService = {
+    useUserProfile,
+};
+
+export default eventChanelService;
