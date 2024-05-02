0230f07ca61d7d07f4810b7c836763b2f6ef4eb0
Amin <Amin@nematifamilyfundation.onmicrosoft.com>
2024-05-02 23:25:12 +0330

    UPDATE: chat page

--- src/app/[lang]/(protect-roots)/(apps)/chat/page.tsx
+++ src/app/[lang]/(protect-roots)/(apps)/chat/page.tsx
@@ -3,10 +3,10 @@ import type { LangParams } from "@/services/types";
 import ChatLoading from "@/app/[lang]/(protect-roots)/(apps)/chat/loading";
 import dynamic from "next/dynamic";
 
-const ChatPage = dynamic(() => import("@/components/pages/chat-pdf"), {
+const ChatPage = dynamic(() => import("@/components/pages/chat"), {
   loading: () => <ChatLoading />,
 });
 
 export default function Chat({ params: { lang } }: LangParams) {
-  return <ChatPage />;
+  return <ChatPage lang={lang} />;
 }
