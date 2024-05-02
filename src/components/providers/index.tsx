3ce0efd1986c8107653b9949de1d6611c17b6606
Amin <Amin@nematifamilyfundation.onmicrosoft.com>
2024-05-02 16:20:08 +0330

    UPDATE: add App Progress Bar in routing

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React, { useEffect } from "react";
 import { DirectionProvider } from "@radix-ui/react-direction";
-
+import { AppProgressBar } from "next-nprogress-bar";
 import { EditorContextProvider } from "@/stores/contexts/editor-context";
 
 import { useChangeDirection } from "@/hooks";
 import { dirInLocalStorage } from "@/stores/browser-storage";
 import { ReactQueryProvider } from "./ReactQueryProvider";
+import { NextAuthProvider } from "@/components/providers/NextAuthProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 /**
  * all providers of app
@@ -19,11 +21,20 @@ export function Providers({ children }: { children: React.ReactNode }) {
   useInitialSetDirToHtmlTag();
 
   return (
-    <ReactQueryProvider>
-      <EditorContextProvider>
-        <DirectionProvider dir={dirState}>{children}</DirectionProvider>
-      </EditorContextProvider>
-    </ReactQueryProvider>
+    <NextAuthProvider>
+      <ReactQueryProvider>
+        <EditorContextProvider>
+          <DirectionProvider dir={dirState}>{children}</DirectionProvider>
+          <AppProgressBar
+            height="4px"
+            color="hsl(256, 78%, 69%)"
+            options={{ showSpinner: false }}
+            shallowRouting
+          />
+          <Toaster />
+        </EditorContextProvider>
+      </ReactQueryProvider>
+    </NextAuthProvider>
   );
 }
 
