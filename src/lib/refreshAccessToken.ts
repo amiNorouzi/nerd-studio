f62f8d6e55ca3f9516d8748954eb379953c2587e
RezaZeraatkarNerdStudio <rezaz@nematifamilyfundation.onmicrosoft.com>
2024-05-06 09:53:16 +0330

    temp save - refreshAccessToken

--- src/lib/refreshAccessToken.ts
+++ src/lib/refreshAccessToken.ts
@@ -1,41 +1,32 @@
 import axiosClient from "@/services/axios-client";
+import type { JWT } from "next-auth/jwt";
 
 export async function refreshAccessToken(token:any) {
   console.log("Refreshing access token");
+
   try {
-    const {data} = await axiosClient.post<
-      unknown,
-      {data: {access_token:string} | {message: string}},
-      {refresh_token:string;}
-    >("/auth/refresh/", {refresh_token: token.refreshToken});
+    const { data } = await axiosClient.post("/auth/refresh/", { refresh_token: token.refreshToken });
 
-    let accessToken = null;
-    let message = null;
-
-    if('access_token' in data)
-      accessToken = data.access_token;
-    else message = data.message;
-
-    // error ocuured during refreshToken response
-    if(message) {
-      throw message;
+    if ('message' in data) {
+      throw new Error(data.message);
     }
 
-    console.log("Refreshed access token");
-    console.log(accessToken);
+    const { access_token, refresh_token } = data;
+
+    console.log("Refreshed access token", access_token, refresh_token);
 
     return {
       ...token,
-      accessToken: accessToken,
-      accessTokenExpires: Date.now() + 5*60*1000,
-      refreshToken: accessToken ?? token.refreshToken, // Fall back to old refresh token
+      accessToken: access_token,
+      accessTokenExpires: Date.now() + 5 * 60 * 1000,
+      refreshToken: refresh_token ?? token.refreshToken, // Fall back to old refresh token
     };
   } catch (error) {
-    console.log(error);
+    console.error(error);
 
     return {
       ...token,
-      error: "RefreshAccessTokenError" as const,
+      error: "RefreshAccessTokenError",
     };
   }
 }
