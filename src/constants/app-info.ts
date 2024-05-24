5d52e0b854809c75e7ece38ad08c7e72d797716c
Amin <Amin@nematifamilyfundation.onmicrosoft.com>
2024-05-24 21:20:43 +0330

    RELEASE: version 0.3.2-2024-5-24

--- src/constants/app-info.ts
+++ src/constants/app-info.ts
@@ -2,4 +2,4 @@ export const APP_NAME = "Nerd Studio";
 export const APP_DEFAULT_TITLE = "Nerd Studio | Home";
 export const APP_TITLE_TEMPLATE = "Nerd Studio | %s";
 export const APP_DESCRIPTION = "Nerd Studio AI";
-export const APP_VERSION = "0.3.1-2024-5-24";
+export const APP_VERSION = "0.3.2-2024-5-24";
